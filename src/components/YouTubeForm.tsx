23c4567f9ea895c69929d4b1d4d72371192091eb
amontalvof <amontalvof92@gmail.com>
2024-06-12 21:57:42 -0400

    feat: Managing Form State

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,16 +1,21 @@
+import { useForm } from 'react-hook-form';
+
 const YouTubeForm = () => {
+    const form = useForm();
+    const { register } = form;
+
     return (
         <div>
             <h1>YouTube Form</h1>
             <form>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+                <input type="text" id="username" {...register('username')} />
                 <br />
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" {...register('email')} />
                 <br />
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" />
+                <input type="text" id="channel" {...register('channel')} />
                 <br />
                 <button>Submit</button>
             </form>
