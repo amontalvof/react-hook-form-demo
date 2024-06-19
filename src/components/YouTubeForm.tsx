d36a0ad26bde81582d5d2ece4d8bd3ba9df06939
amontalvof <amontalvof92@gmail.com>
2024-06-18 23:10:03 -0400

    feat: Form Validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -21,15 +21,46 @@ const YouTubeForm = () => {
     return (
         <div>
             <h1>YouTube Form ({renderCount / 2})</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register('username')} />
+                <input
+                    type="text"
+                    id="username"
+                    {...register('username', {
+                        required: {
+                            value: true,
+                            message: 'Username is required!',
+                        },
+                    })}
+                />
                 <br />
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register('email')} />
+                <input
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                        required: {
+                            value: true,
+                            message: 'Email is required!',
+                        },
+                        pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            message: 'Invalid email format!',
+                        },
+                    })}
+                />
                 <br />
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register('channel')} />
+                <input
+                    type="text"
+                    id="channel"
+                    {...register('channel', {
+                        required: {
+                            value: true,
+                            message: 'Chanel is required!',
+                        },
+                    })}
+                />
                 <br />
                 <button>Submit</button>
             </form>
