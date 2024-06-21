b63b30423c260a9acb3ecb2ae1a56be8e0c03920
amontalvof <amontalvof92@gmail.com>
2024-06-21 18:53:13 -0400

    feat: Default Values

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -10,7 +10,13 @@ type FormValues = {
 };
 
 const YouTubeForm = () => {
-    const form = useForm<FormValues>();
+    const form = useForm<FormValues>({
+        defaultValues: {
+            username: '',
+            email: '',
+            channel: '',
+        },
+    });
     const { register, control, handleSubmit, formState } = form;
     const { errors } = formState;
 
